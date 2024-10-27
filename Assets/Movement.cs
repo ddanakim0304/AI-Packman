c687c8e92309f241204d4abd1fd4f150e2aa0ec3
Dain <kim@uni.minerva.edu>
2024-10-27 17:04:51 -0300

    Update Movement.cs

--- Assets/Movement.cs
+++ Assets/Movement.cs
@@ -11,11 +11,64 @@ public class Movement : MonoBehaviour
     public Vector2 initialDirection;
     public LayerMask obsticleLayer;
     public Rigidbody2D rb { get; private set; }
-
+    public Vector2 direction { get; private set; }
+    public Vector2 nextDirection { get; private set; }
+    public Vector3 startingPosition { get; private set; }
     private void Awake()
     {
         this.rb = GetComponent<Rigidbody2D>();
+        this.startingPosition = this.transform.position;
     }
 
+    private void Start()
+    {
+        ResetState();
+    }
+
+    public void ResetState()
+    {
+        this.speedMultiplier = 1.0f;
+        this.direction = this.initialDirection;
+        this.nextDirection = Vector2.zero;
+        this.transform.position = this.startingPosition;
+        this.rb.isKinematic = false;
+        this.enabled = true;
+    }
+
+
+    private void Update()
+    {
+        if (this.nextDirection != Vector2.zero){
+            SetDirection(this.nextDirection);
+        }
+    }
+
+
+    private void FixedUpdate()
+    {
+        Vector2 position = this.rb.position;
+        Vector2 translation = this.direction * this.speed * this.speedMultiplier * Time.fixedDeltaTime;
+        
+        this.rb.MovePosition(position + translation);
+    }
+
+    public void SetDirection(Vector2 direction, bool forced = false)
+    {
+        if (forced || !Occupied(direction))
+        {
+            this.direction = direction;
+            this.nextDirection = Vector2.zero;
+        }   
+        else
+        {
+            this.nextDirection = direction;
+        }
+    }
+
+    public bool Occupied(Vector2 direction)
+    {
+        RaycastHit2D hit = Physics2D.BoxCast(this.transform.position, Vector2.one * 0.75f, 0.0f, direction, 1.5f, this.obsticleLayer);
+        return hit.collider != null;
+    }
 
 }
