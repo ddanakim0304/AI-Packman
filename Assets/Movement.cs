1800d7363fc8e18bebc209792e3ea5782dfe41ee
Dain <kim@uni.minerva.edu>
2024-10-27 17:21:47 -0300

    debug Movement script

--- Assets/Movement.cs
+++ Assets/Movement.cs
@@ -1,23 +1,22 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 [RequireComponent(typeof(Rigidbody2D))]
 public class Movement : MonoBehaviour
 {
-
-    public float speed = 8.0f;
-    public float speedMultiplier = 1.0f;
+    public float speed = 8f;
+    public float speedMultiplier = 1f;
     public Vector2 initialDirection;
-    public LayerMask obsticleLayer;
+    public LayerMask obstacleLayer;
+
     public Rigidbody2D rb { get; private set; }
     public Vector2 direction { get; private set; }
     public Vector2 nextDirection { get; private set; }
     public Vector3 startingPosition { get; private set; }
+
     private void Awake()
     {
-        this.rb = GetComponent<Rigidbody2D>();
-        this.startingPosition = this.transform.position;
+        rb = GetComponent<Rigidbody2D>();
+        startingPosition = transform.position;
     }
 
     private void Start()
@@ -27,47 +26,51 @@ public class Movement : MonoBehaviour
 
     public void ResetState()
     {
-        this.speedMultiplier = 1.0f;
-        this.direction = this.initialDirection;
-        this.nextDirection = Vector2.zero;
-        this.transform.position = this.startingPosition;
-        this.rb.isKinematic = false;
-        this.enabled = true;
+        speedMultiplier = 1f;
+        direction = initialDirection;
+        nextDirection = Vector2.zero;
+        transform.position = startingPosition;
+        rb.isKinematic = false;
+        enabled = true;
     }
-
 
     private void Update()
     {
-        if (this.nextDirection != Vector2.zero){
-            SetDirection(this.nextDirection);
+        // Try to move in the next direction while it's queued to make movements
+        // more responsive
+        if (nextDirection != Vector2.zero) {
+            SetDirection(nextDirection);
         }
     }
 
-
     private void FixedUpdate()
     {
-        Vector2 position = this.rb.position;
-        Vector2 translation = this.direction * this.speed * this.speedMultiplier * Time.fixedDeltaTime;
-        
-        this.rb.MovePosition(position + translation);
+        Vector2 position = rb.position;
+        Vector2 translation = speed * speedMultiplier * Time.fixedDeltaTime * direction;
+
+        rb.MovePosition(position + translation);
     }
 
     public void SetDirection(Vector2 direction, bool forced = false)
     {
+        // Only set the direction if the tile in that direction is available
+        // otherwise we set it as the next direction so it'll automatically be
+        // set when it does become available
         if (forced || !Occupied(direction))
         {
             this.direction = direction;
-            this.nextDirection = Vector2.zero;
-        }   
+            nextDirection = Vector2.zero;
+        }
         else
         {
-            this.nextDirection = direction;
+            nextDirection = direction;
         }
     }
 
     public bool Occupied(Vector2 direction)
     {
-        RaycastHit2D hit = Physics2D.BoxCast(this.transform.position, Vector2.one * 0.75f, 0.0f, direction, 1.5f, this.obsticleLayer);
+        // If no collider is hit then there is no obstacle in that direction
+        RaycastHit2D hit = Physics2D.BoxCast(transform.position, Vector2.one * 0.75f, 0f, direction, 1.5f, obstacleLayer);
         return hit.collider != null;
     }
 
