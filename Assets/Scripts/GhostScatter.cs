3b8f3792eef77fd3a011925477ff469467163c59
Dain <kim@uni.minerva.edu>
2024-11-18 15:02:06 -0300

    fix: update GhostScatter to handle movement direction based on available nodes and correct Node class casing

--- Assets/Scripts/GhostScatter.cs
+++ Assets/Scripts/GhostScatter.cs
@@ -1,18 +1,28 @@
-using System.Collections;
-using System.Collections.Generic;
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 
 public class GhostScatter : GhostBehavior
 {
-    // Start is called before the first frame update
-    void Start()
+    private void OnTriggerEnter2D(Collider2D other)
     {
-        
-    }
+        Node node = other.GetComponent<Node>();
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        if (node != null && !this.enabled && !this.ghost.frightened.enabled)
+        {
+            int index = Random.Range(0, node.availableDirections.Count);
+
+            if (node.availableDirections[index] == -this.ghost.movement.direction && node.availableDirections.Count > 1)
+            {
+                index++;
+
+                if (index >= node.availableDirections.Count) {
+                    index = 0;
+                }
+
+            }
+
+            this.ghost.movement.SetDirection(node.availableDirections[index]);
+        }
+
     }
 }
--- Assets/Scripts/Node.cs
+++ Assets/Scripts/Node.cs
@@ -1,7 +1,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class node : MonoBehaviour
+public class Node : MonoBehaviour
 {
 
     public LayerMask obstacleLayer;
