077950a8415edd815c93c650f28047932413ea45
Dain <kim@uni.minerva.edu>
2024-11-18 16:23:03 -0300

    feat: implement ghost frightened behavior

--- Assets/Scripts/GhostFrightened.cs
+++ Assets/Scripts/GhostFrightened.cs
@@ -1,18 +1,107 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class GhostFrightened : GhostBehavior
 {
-    // Start is called before the first frame update
-    void Start()
+    public SpriteRenderer body;
+    public SpriteRenderer eyes;
+    public SpriteRenderer blue;
+    public SpriteRenderer white;
+
+    private bool eaten;
+
+    public override void Enable(float duration)
     {
-        
+        base.Enable(duration);
+
+        body.enabled = false;
+        eyes.enabled = false;
+        blue.enabled = true;
+        white.enabled = false;
+
+        Invoke(nameof(Flash), duration / 2f);
     }
 
-    // Update is called once per frame
-    void Update()
+    public override void Disable()
     {
-        
+        base.Disable();
+
+        body.enabled = true;
+        eyes.enabled = true;
+        blue.enabled = false;
+        white.enabled = false;
     }
+
+    private void Eaten()
+    {
+        eaten = true;
+        ghost.SetPosition(ghost.home.inside.position);
+        ghost.home.Enable(duration);
+
+        body.enabled = false;
+        eyes.enabled = true;
+        blue.enabled = false;
+        white.enabled = false;
+    }
+
+    private void Flash()
+    {
+        if (!eaten)
+        {
+            blue.enabled = false;
+            white.enabled = true;
+            white.GetComponent<AnimatedSprites>().Restart();
+        }
+    }
+
+    private void OnEnable()
+    {
+        blue.GetComponent<AnimatedSprites>().Restart();
+        ghost.movement.speedMultiplier = 0.5f;
+        eaten = false;
+    }
+
+    private void OnDisable()
+    {
+        ghost.movement.speedMultiplier = 1f;
+        eaten = false;
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        Node node = other.GetComponent<Node>();
+
+        if (node != null && enabled)
+        {
+            Vector2 direction = Vector2.zero;
+            float maxDistance = float.MinValue;
+
+            // Find the available direction that moves farthest from pacman
+            foreach (Vector2 availableDirection in node.availableDirections)
+            {
+                // If the distance in this direction is greater than the current
+                // max distance then this direction becomes the new farthest
+                Vector3 newPosition = transform.position + new Vector3(availableDirection.x, availableDirection.y);
+                float distance = (ghost.pacman.position - newPosition).sqrMagnitude;
+
+                if (distance > maxDistance)
+                {
+                    direction = availableDirection;
+                    maxDistance = distance;
+                }
+            }
+
+            ghost.movement.SetDirection(direction);
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.layer == LayerMask.NameToLayer("Pacman"))
+        {
+            if (enabled) {
+                Eaten();
+            }
+        }
+    }
+
 }
